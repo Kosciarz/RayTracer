ef2c501ae4737b3dac280fc5708eebbc681321dc
Bartosz <bartoszbachul1@gmail.com>
2025-02-08 23:36:37 +0100

    created a ppm test image

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,39 @@
-#include "raylib.h"
+// #include "raylib.h"
 
 #include <iostream>
+#include <fstream>
+
 
 int main()
 {
-    constexpr int width = 800;
-    constexpr int height = 450;
+    
+    constexpr int image_width = 256;
+    constexpr int image_height = 256;
 
-    InitWindow(800, 450, "raylib windows");
-
-    while (!WindowShouldClose())
+    std::ofstream file("../image.ppm");
+    if (!file.is_open())
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
+        std::cerr << "Error opening file!" << '\n';
+        return -1;
     }
 
-    CloseWindow();
+   file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int i = 0; i < image_height; ++i)
+    {
+        for (int j = 0; j < image_width; j++)
+        {
+            auto red = static_cast<double>(i) / (image_width - 1);
+            auto green = static_cast<double>(j) / (image_height - 1);
+            auto blue = 0.0;
+
+            int ir = int(255.999 * red);
+            int ig = int(255.999 * green);
+            int ib = int(255.999 * blue);
+
+            file << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    file.close();
 }
