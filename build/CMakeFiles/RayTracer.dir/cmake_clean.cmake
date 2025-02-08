ab5f517e6cb5218cefe5872b2849a89d0f400910
Bartosz <bartoszbachul1@gmail.com>
2025-02-08 13:23:03 +0100

    initalize project

---
+++ build/CMakeFiles/RayTracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracer.dir/src/Main.cpp.obj"
+  "CMakeFiles/RayTracer.dir/src/Main.cpp.obj.d"
+  "RayTracer.exe"
+  "RayTracer.exe.manifest"
+  "RayTracer.pdb"
+  "libRayTracer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
